9bf658f67141b697201931ee84b93a848b6ecc28
DimiyaJr <dimanrandisa@gmail.com>
2022-03-15 09:06:20 +0530

    Quiz is Almost Done.
    Small problem of Used time. That's the issue. Other things are correct.

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -7,6 +7,7 @@ function on(){
 function mark(){
 	var a=0;
 	var b=0;
+	var r=0;
 	var qt1=document.qz.q1.value;
 	var qt2=document.qz.q2.value;
 	var qt3=document.qz.q3.value;
@@ -19,44 +20,53 @@ function mark(){
 	var qt10=document.qz.q10.value;
     var out=document.getElementById('out');
 	
-	if (qt1=="lib3"){a= a+2}
+	if (qt1=="lib3"){a= a+2,r++}
 	else{b++}
-	if (qt2=="lib23"){a= a+2}
+	if (qt2=="lib23"){a= a+2,r++}
 	else{b++}
-	if (qt3=="lib34"){a= a+2}
+	if (qt3=="lib34"){a= a+2,r++}
 	else{b++}
-	if (qt4=="lib44"){a= a+2}
+	if (qt4=="lib44"){a= a+2,r++}
 	else{b++}
-	if (qt5=="lib54"){a= a+2}
+	if (qt5=="lib54"){a= a+2,r++}
 	else{b++}
-	if (qt6=="lib62"){a= a+2}
+	if (qt6=="lib62"){a= a+2,r++}
 	else{b++}
-	if (qt7=="lib74"){a= a+2}
+	if (qt7=="lib74"){a= a+2,r++}
 	else{b++}
-	if (qt8=="lib83"){a= a+2}
+	if (qt8=="lib83"){a= a+2,r++}
 	else{b++}
-	if (qt9=="lib93"){a= a+2}
+	if (qt9=="lib93"){a= a+2,r++}
 	else{b++}
-	if (qt10=="lib101"){a= a+2}
+	if (qt10=="lib101"){a= a+2,r++}
 	else{b++}
 
-	if(a<=3){
-		out.textContent=`Your result is ${a-b}. Work Hard.` 
+	if(a-b<8){
+		out.textContent=`Your result is ${a-b}. Work Hard. You only got correct ${r} out of 10 Questions.`
+		document.body.style.backgroundColor = "#ff0000"; 
 	}
 	else{
-		out.textContent=`Your result is ${a-b}. Keep it up!`
+		out.textContent=`Your result is ${a-b}. Keep it up!. You only got correct ${r} out of 10 Questions.`
+		document.body.style.backgroundColor = "#00ff00";
 	}
 }
 
 function timer001(){
 	c = c -1;
 	if (c<60){
-		time001.innerHTML = c; 
+		time001.innerHTML = c;
+		document.getElementById('rtime').innerHTML= c;
 	}
 
 	if(c<1){
 		window.clearInterval(update);
+		document.getElementById('rtime').innerHTML= c;
 	}
+
+	if(c===0){
+		document.getElementById('count').innerHTML='Time is Up';
+	}
+
 }
 
 update = setInterval("timer001()",1000);
