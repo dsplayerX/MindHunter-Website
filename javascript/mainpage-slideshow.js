c61dbe5248b0281ea30ad56a994d3be4a836c3e1
dsplayerX <dumindu0043@gmail.com>
2022-03-18 21:55:14 +0530

    Added a slideshow to main page

---
+++ javascript/mainpage-slideshow.js
@@ -0,0 +1,16 @@
+let i = 0;
+let images = ["images/quotes/a-mind-needs-a-book.jpg", "images/quotes/good-books-dont-give.jpg", "images/quotes/a-reader-lives-a-thousand.jpg", "images/quotes/life-is-a-book.jpg", "images/quotes/never-trust-anyone-who-hasnt.jpg", "images/quotes/sleep-good-books-better.jpg", "images/quotes/today-reader-tomorrow-leader.jpg"];
+
+function changeImage(){
+    document.slides.src = images[i];
+
+    if (i < images.length -1) {
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImage()", 3500);
+}
+
+window.onload = changeImage;
