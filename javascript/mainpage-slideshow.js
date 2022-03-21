55e61d53687a3ee67f209fb4693d57ea41f2ada1
dsplayerX <dumindu0043@gmail.com>
2022-03-21 21:08:13 +0530

    Added my photo, RESIZED all images, Moved a file into correct path
    RESIZED ALL EDITOR IMAGES TO 4:5 aspect ratio so it looks more consistant.
    Moved and renamed 2 of nanduni's files to correct paths. Linked store.js to store.html

--- javascript/mainpage-slideshow.js
+++ javascript/mainpage-slideshow.js
@@ -1,3 +1,4 @@
+//Main Page Slideshow JS - Dumindu//
 let i = 0;
 let images = ["images/quotes/a-mind-needs-a-book.jpg", "images/quotes/good-books-dont-give.jpg", "images/quotes/a-reader-lives-a-thousand.jpg", "images/quotes/life-is-a-book.jpg", "images/quotes/never-trust-anyone-who-hasnt.jpg", "images/quotes/sleep-good-books-better.jpg", "images/quotes/today-reader-tomorrow-leader.jpg"];
 
