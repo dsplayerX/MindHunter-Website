b0b29bc6df16ad0b76ca4f7b1ce9fb6627607e78
dsplayerX <dumindu0043@gmail.com>
2022-03-23 18:37:47 +0530

    changed slideshow code and js

--- javascript/mainpage-slideshow.js
+++ javascript/mainpage-slideshow.js
@@ -3,7 +3,7 @@ let i = 0;
 let images = ["images/quotes/a-mind-needs-a-book.jpg", "images/quotes/good-books-dont-give.jpg", "images/quotes/a-reader-lives-a-thousand.jpg", "images/quotes/life-is-a-book.jpg", "images/quotes/never-trust-anyone-who-hasnt.jpg", "images/quotes/sleep-good-books-better.jpg", "images/quotes/today-reader-tomorrow-leader.jpg"];
 
 function changeImage(){
-    document.slides.src = images[i];
+    document.getElementById("slides").src = images[i];
 
     if (i < images.length -1) {
         i++;
